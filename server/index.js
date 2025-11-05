672ce782e52cfde7a0d0c9909f099b78b1d3f2a6
basant-specs <basantdhiman9@gmail.com>
2025-11-05 20:26:32 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,68 +1,50 @@
 const express = require('express');
-const app = express();
-
-
-//we have to do http handshake for socket io
 const http = require('http');
-const express_server = http.createServer(app);
-
-
-//socket.io server programming starts here 
-//setup cors 
-const cors = require('cors');
-app.use(cors());
-
-//setting up socket.io
 const { Server } = require('socket.io');
+const cors = require('cors');
 
-const io = new Server(express_server, {
-    cors:{
-        origin:"*", //we'll use * for now but in production we should use our client url example http://amazon.com or http://myapp.com 
-        methods:["GET", "POST"]
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST"]
+}));
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST"]
     }
-})
-
-//listen on every connection
-//event handlers -> emit, on 
-//predefined events -> connection, disconnect
-io.on("connection",
-    (socket)=>{
-        console.log(`User connected: ${socket.id}`);
-
-        //Join a room
-        socket.on("join_room", (room)=>{
-            socket.join(room);
-            console.log(`User with ID: ${socket.id} joined room: ${room}`);
-        })
-
-
-        //Recieve the 'message' from the client
-        socket.on('message', (data)=>{
-
-            const msg = `Message from ${socket.id} : ${data.message} for ${data.reciever}`;
-            console.log(msg);
-            //Broadcast the message to all the clients except the sender
-            // socket.broadcast.emit('message', msg)
-            
-
-            
-            //Send to specific client
-            if(data.reciever){
-                socket.to(data.reciever).emit('message', data.message);
-            }else{
-                socket.broadcast.emit('message', data.message)
-            }
-        })
-
-
-    }
-)
-
-app.get("/", (req, res)=>{
-    res.send("Hello World");
-})
-
-
-express_server.listen(3000, () => {
-  console.log('Server is running on port 3000');
 });
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("join_room", (room) => {
+        socket.join(room);
+        console.log(`User with ID: ${socket.id} joined room: ${room}`);
+    });
+
+    socket.on("message", (data) => {
+        const msg = `Message from ${socket.id} : ${data.message} for ${data.reciever}`;
+        console.log(msg);
+
+        if (data.reciever) {
+            socket.to(data.reciever).emit('message', data.message);
+        } else {
+            socket.broadcast.emit('message', data.message);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
